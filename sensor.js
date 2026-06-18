6e6c6d37d7148459921ca00f6dbde8b5ae2ef6ed
Lol12345628 <111190537+Lol12345628@users.noreply.github.com>
2026-06-18 08:58:49 +0700

    Add files via upload

--- sensor.js
+++ sensor.js
@@ -18,36 +18,25 @@ export default function handler(req, res) {
     let reason = "สภาพปกติ";
     let confidence = 90;
 
-    // 🌱 soil priority
     if (soil < 30) {
       ai = "PUMP ON";
       reason = "ดินแห้ง → ต้องรดน้ำ";
       confidence = 93;
     }
 
-    // 🌡 temp override
     if (temp > 35) {
       ai = "FAN ON";
       reason = "อุณหภูมิสูง → เปิดพัดลม";
       confidence = 95;
     }
 
-    // 💡 light
     if (light < 100) {
       ai = "LIGHT ON";
-      reason = "แสงน้อย → เปิดไฟช่วยพืช";
+      reason = "แสงน้อย → เปิดไฟ";
       confidence = 88;
     }
 
-    lastData = {
-      temp,
-      hum,
-      soil,
-      light,
-      ai,
-      reason,
-      confidence
-    };
+    lastData = { temp, hum, soil, light, ai, reason, confidence };
 
     return res.json({ ok: true, data: lastData });
   }
